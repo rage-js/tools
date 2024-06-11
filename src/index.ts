0ed8996ce3200c420781f5f3456a220c2ba27447
Maghish <mail4mithran@gmail.com>
2024-06-11 19:20:52 +0530

    Created index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * The tool kit class consisting all the tool functions
+ */
+class ToolKit {
+  configPath: string;
+
+  constructor(configPath: string) {
+    this.configPath = configPath;
+  }
+}
+
+export { ToolKit };
